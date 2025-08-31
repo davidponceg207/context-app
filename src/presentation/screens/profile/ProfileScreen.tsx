cbb66217685a800ac9fce2d14cec71b9c814832f
David <davidponceg207@gmail.com>
2025-08-31 10:21:54 -0600

    Change state

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import { styles } from '../../../config'
 import { useProfileStore } from '../../store/profile-store'
 
@@ -7,12 +7,31 @@ export const ProfileScreen = () => {
 
     const name = useProfileStore(state => state.name);
     const email = useProfileStore(state => state.email);
+    const changeProfile = useProfileStore(state => state.changeProfile);
 
 
     return (
         <View style={ styles.container }>
             <Text style={styles.title}>{name}</Text>
             <Text style={styles.title}>{email}</Text>
+
+            <Pressable
+                onPress={ () => useProfileStore.setState({ name: 'David'})}
+                style={ styles.primaryButton }>
+                <Text>Change Name</Text>
+            </Pressable>
+
+            <Pressable
+                onPress={ () => useProfileStore.setState({ email: 'david@mail.com'})}
+                style={ styles.primaryButton }>
+                <Text>Change Email</Text>
+            </Pressable>
+
+            <Pressable
+                onPress={ () => changeProfile('Dua', 'dua@mail.com')}
+                style={ styles.primaryButton }>
+                <Text>Return</Text>
+            </Pressable>
         </View>
     )
 }
--- src/presentation/store/profile-store.ts
+++ src/presentation/store/profile-store.ts
@@ -4,9 +4,16 @@ import { create } from "zustand";
 export interface ProfileState {
     name: string;
     email: string;
+
+    changeProfile: (name: string, email: string) => void;
 }
 
 export const useProfileStore = create<ProfileState>()( (set, get) => ({
     name: 'Dua Lipa',
-    email: 'dua@mail.com'
+    email: 'dua@mail.com',
+
+    changeProfile: (name: string, email: string) => {
+        console.log(get())
+        set({name, email});
+    }
 }))
